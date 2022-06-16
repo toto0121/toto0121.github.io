1f115069def9e8546d30077554134799ea219d7f
toto0121 <44390017+toto0121@users.noreply.github.com>
2022-06-16 18:50:12 +0900

    toHint_color

--- common.js
+++ common.js
@@ -1,12 +1,16 @@
-addButton("rect", ["x", 50, "y", 50, "width", 250, "height", 100, "stroke", "black", "ontouchstart", "toHome()"]);
-addText(svg, "とと謎一覧へ",["x",175, "y", 120, "font-size", 40, "pointer-events", "none"]);
+const pushButton = (event) =>{
+    event.target.setAttribute("fill", "#AAAAAA");
+}
+
+addButton("rect", ["x", 50, "y", 50, "width", 250, "height", 100, "stroke", "black", "ontouchstart", "pushButton(evt)", "onclick", "toHome()"]);
+addText(svg, "とと謎一覧へ",["x",175, "y", 115, "font-size", 35, "pointer-events", "none"]);
 
 const toHome = () => {
     window.location.href = "nazo_index.html";
 }
 
-addButton("rect", ["x", 50, "y", 180, "width", 250, "height", 100, "stroke", "black","ontouchstart", "toHint()"]);
-addText(svg, "ヒント・解説",["x",175, "y", 250, "font-size", 40, "pointer-events", "none"]);
+addButton("rect", ["x", 50, "y", 180, "width", 250, "height", 100, "stroke", "black","ontouchstart", "pushButton(evt)", "onclick", "toHint()"]);
+addText(svg, "ヒント・解説",["x",175, "y", 245, "font-size", 35, "pointer-events", "none"]);
 
 const toHint = () => {
     window.location.href = contentName + "_hint.html";
