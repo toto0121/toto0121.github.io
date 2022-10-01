2821c1a9de1934f3150a025d4ee36949641f370d
toto0121 <44390017+toto0121@users.noreply.github.com>
2022-10-01 15:12:39 +0900

    a

--- heart copy.js
+++ heart copy.js
@@ -165,11 +165,11 @@ function clearEvt()
     camera.appendChild(tweetdiv);
 
     let twtTxt = "見事" + contentName + "の謎を解き明かした！%0a%0a#web謎解き #とと謎" + contentName + " #とと謎%0a" + "https://toto0121.github.io/" + contentName + ".html";
-    
+    /*
     tweetdiv.innerHTML = '<iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" '+
     'class="twitter-share-button-rendered twitter-tweet-button" data-url="a" style="position: static; visibility: visible;' + 
     'width: 81px; height: 28px; transform: scale(2);" title="Twitter Tweet Button" src="https://platform.twitter.com/widgets/tweet_button.7dae38096d06923d683a2a807172322a.ja.html&amp;text=' + twtTxt + '&amp;url="></iframe>';
-
+    */
     //tweetdiv.innerHTML = '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
 }
 
