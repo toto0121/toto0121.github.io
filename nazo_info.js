2ed8b9196b95b93b7e76f6e12736b96990b6f921
toto0121 <44390017+toto0121@users.noreply.github.com>
2022-10-14 09:02:35 +0900

    hint

--- nazo_info.js
+++ nazo_info.js
@@ -10,4 +10,4 @@ const grid = new infoObj("grid", "2022年10月7日", ["1.3", "1.2", "1.1", "0.7"
 let allInfo = {};
 allInfo.grid = grid;
 allInfo.sort = new infoObj("sort", "2022年10月14日", ["1.3", "1.2", "1.1", "0.7"]);
-allInfo.art = new infoObj("art", "2022年10月14日", ["1.3", "1.2", "1.1", "0.7"]);
+allInfo.art = new infoObj("art", "2022年10月14日", ["1.2", "1.0", "0.9", "0.6"]);
